573e46bced609e92e3dcd4c27e9cc0c53e5f3044
shimizu <jshimizujp@gmail.com>
2018-03-14 21:46:20 +0900

    [fix] fix templates path in /

--- web.py
+++ web.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/' ,  methods=["GET"])
 def hello():
-    return render_template("templates/index.html")
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
