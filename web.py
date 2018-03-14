0edcdd20453914c40245032ac5f026d5c741af7e
shimizu <jshimizujp@gmail.com>
2018-03-14 21:44:26 +0900

    [feature] implement page with template

--- web.py
+++ web.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
+
 app = Flask(__name__)
 
 
 
 @app.route('/' ,  methods=["GET"])
 def hello():
-    return "Hello World! this is the first step of Kremlin web!"
+    return render_template("templates/index.html")
 
 if __name__ == '__main__':
     app.run()
